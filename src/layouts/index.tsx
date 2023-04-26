b4756474c0e911729a4368bc017f0cfd4e514c43
debuginn <debuginn@icloud.com>
2023-04-26 09:52:51 +0800

    feat add file

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,11 @@
-import { Link, Outlet } from 'umi';
-import styles from './index.less';
+import {Outlet} from 'umi';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Layout() {
-  return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
-  );
+    return (
+        <div>
+            <Outlet/>
+        </div>
+    );
 }
