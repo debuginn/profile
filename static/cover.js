a9a8f350a8ace916f41c2815b9c7f3257af52d0b
debuginn <debuginn@icloud.com>
2023-12-24 15:14:26 +0800

    add 2023 top 10 image

--- static/cover.js
+++ static/cover.js
@@ -8,13 +8,17 @@ bodyBgs[5] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/20230312001814
 bodyBgs[6] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162149771.jpg";  // 飞上云霄
 bodyBgs[7] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162151707.jpg";  // 洱海的月
 bodyBgs[8] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162152763.jpg";  // 和她她她
-bodyBgs[9] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162152732.jpg"; // 松赞林寺
+bodyBgs[9] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162152732.jpg";  // 松赞林寺
 bodyBgs[10] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162153714.jpg"; // 滇池海鸥
 bodyBgs[11] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162153417.jpg"; // 蓝月谷
 bodyBgs[12] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162154852.jpg"; // 玉龙雪山
 bodyBgs[13] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162154677.jpg"; // 大冰的小屋
 bodyBgs[14] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162155271.jpg"; // 小狗老板
 bodyBgs[15] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202305162155529.jpg"; // 苍山的云
+bodyBgs[16] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202312241440666.jpg"; // 夜爬东灵山
+bodyBgs[17] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202312241448297.jpg"; // 伏地魔十七
+bodyBgs[18] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202312241459045.jpg"; // 搁浅的布鲁维斯号
+bodyBgs[19] = "https://cdn.jsdelivr.net/gh/debuginn/image@main/img/202312241509754.jpg"; // 青岛的浪
 
 // 输出随机的背景图
 const randomBgIndex = Math.round(Math.random() * (bodyBgs.length - 1));
